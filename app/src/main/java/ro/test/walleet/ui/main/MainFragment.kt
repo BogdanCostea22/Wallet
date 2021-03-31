7c5a9a7482ce9f5ea2303d080bebb518c242f4fb
Bogdan Costea <bogdancostea22@gmail.com>
2021-03-31 18:28:36 +0300

    introduced fragment ktx library

--- app/src/main/java/ro/test/walleet/ui/main/MainFragment.kt
+++ app/src/main/java/ro/test/walleet/ui/main/MainFragment.kt
@@ -1,27 +1,23 @@
 package ro.test.walleet.ui.main
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import ro.test.walleet.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
-    private lateinit var viewModel: MainViewModel
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-    }
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentMainBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
 
         return binding.root
     }
